70ba0dd8515b85a90ed0f0a29308e70a183a798c
Jiyao Li <jiyao.li@aggiemail.usu.edu>
2019-10-17 16:51:46 -0600

    improve rider and its UTs

--- Config.py
+++ Config.py
@@ -4,12 +4,22 @@ SERVING  = "serving"
 FINISHED = "finished"
 CANCEL = "cancel"
 
+DETOUR_WEIGH = 0.05
+DISCOUNT_1 = 0.96
+DISCOUNT_2 = 0.93
+DISCOUNT_3 = 0.9
+DISCOUNT_4 = 0.87
+
 #Driver Para
 VEHICLE_CAPACITY = 4
 IDLE = 'idle'
 INSERVICE ='inservice'
+COST_PER_MINUTE = 0.3
+SAT_PRICE = 1.5
+SAT_TIME = 0.8
 
 #Simulation
 FILENAME_R = './Test/Chicago_Apr_11_2016.csv'
+SIMULATION_CYCLE = 3  #every 3 minutes
 RIDER_ROW_START = 0
 RIDER_ROW_END = 1
--- Rider.py
+++ Rider.py
@@ -10,20 +10,21 @@ class Rider:
         self.__startTime = sT
         self.__srcZone = sZ
         self.__destZone = dZ
-        self.__direction = self.__calcDirection(srcX, srcY, destX, destY)
-        self.__defaultPrice = dP
         self.__patience = pat
+        self.__direction = self.__calcDirection(srcX, srcY, destX, destY)
+        self.__shortest_time = Graph.queryTravelCost(str(sZ), str(dZ))
+        self.__default_price = dP
 
         self.__status = WAITING
-        self.__price = 0.0
         self.__wait_time = 0.0
-        self.__detour_time = 0.0
+        self.__travel_time = 0.0
+        self.__price = dP
         self.__sat = 0.0
 
     def __str__(self):
         ret = "{" + str(self.__id) + ', ' + str(self.__startTime) + ", " +  str(self.__srcZone) + ", " + str(self.__destZone) + ", " + \
-                  str(self.__direction) + ", " + str(self.__defaultPrice) +", "+ str(self.__patience) + ", " + str(self.__status) + ", " + \
-                  str(self.__price) + ", " +str(self.__wait_time) + ", " + str(self.__detour_time) +  ", " + str(self.__sat) + "}"
+                  str(self.__patience) + ", " + str(self.__direction) +", "+ str(self.__shortest_time) + ", " + str(self.__default_price) + ", " + \
+                  str(self.__status) + ", " + str(self.__wait_time) + ", " + str(self.__travel_time) +  ", " + str(self.__price) +  ", " + str(self.__sat) + "}"
         return ret
 
     #https://stackoverflow.com/questions/13226038/calculating-angle-between-two-vectors-in-python
@@ -35,46 +36,74 @@ class Rider:
         v0 = np.array(p0) - np.array(p1)
         v1 = np.array(p2) - np.array(p1)
         angle = np.math.atan2(np.linalg.det([v0, v1]), np.dot(v0, v1))
-
         return np.degrees(angle)*(-1)
 
-    def calcDetourTime(self, curr_travel_time):
-        self.__detour_time = curr_travel_time - Graph.queryTravelCost(str(self.__srcZone), str(self.__destZone))
+    def tickWaitTime(self):
+        self.__wait_time += 1
 
+    def getWaitTime(self):
+        return self.__wait_time
 
-    def calcPrice(self, n_shared):
-        self.__price = self.__defaultPrice * 0.9 * math.exp(-0.05*self.__detour_time)
-        if n_shared == 1:
-            self.__price = self.__price * 0.96
-        elif n_shared == 2:
-            self.__price = self.__price * 0.93
-        elif n_shared == 3:
-            self.__price = self.__price * 0.90
-        elif n_shared == 4:
-            self.__price = self.__price * 0.87
-        else:
-            pass
+    def setTravelTime(self, time):
+        self.__travel_time = time
 
-    def calcSatisfaction(self):
-        Wp = random.uniform(1, 1.5)
-        Wt = random.uniform(0.5, 1)
-        self.__sat = math.exp((self.__defaultPrice-self.__price)*Wp) - math.exp(self.__detour_time*Wt) + 1
+    def getTravelTime(self):
+        return self.__travel_time
 
-    def getRiderID(self):
-        return self.__id
+    def getShortestTime(self):
+        return self.__shortest_time
 
     def getDetourTime(self):
-        return self.__detour_time
+        return self.__travel_time - self.__shortest_time
+
+    def calcPrice(self, n_shared):
+        if self.getDetourTime() < 0:
+            return math.inf
+            #log here
+        self.__price = self.__default_price * math.exp(DETOUR_WEIGH * -self.getDetourTime())
+        if n_shared == 1:
+            self.__price = self.__price * DISCOUNT_1
+        elif n_shared == 2:
+            self.__price = self.__price * DISCOUNT_2
+        elif n_shared == 3:
+            self.__price = self.__price * DISCOUNT_3
+        elif n_shared == 4:
+            self.__price = self.__price * DISCOUNT_4
+        else:
+            pass
+            #log here
 
     def getPrice(self):
         return self.__price
 
-#TCs(not remove)
-#uID, sT, sZ, dZ, dP, pat, srcX, srcY, destX, destY
-'''r0=Rider("R0", 0, 8, 24, 7.75, 20, -87.6333, 41.8996, -87.6764, 41.9012)
-r1=Rider("R1", 0, 7, 6, 6, 20, -87.6495, 41.9227, -87.656, 41.9442)
-r2=Rider("R1", 0, 7, 6, 6, 20, 0, 0, -1, 0)
-r0.calcDetourTime(3)
-r0.calcPrice(4)
-r0.calcSatisfaction()
-print(r0)'''
+    def calcSat(self):
+        self.__sat = math.exp((self.__default_price-self.__price)*SAT_PRICE) - \
+                     math.exp(self.getDetourTime()*SAT_TIME) + 1
+        #print(math.exp((self.__default_price-self.__price)*SAT_PRICE))
+        #print(math.exp(self.getDetourTime()*SAT_TIME) - 1)
+
+    def getSat(self):
+        return self.__sat
+
+    def getID(self):
+        return self.__id
+
+    def getStartTime(self):
+        return self.__startTime
+
+    def getSrcZone(self):
+        return self.__srcZone
+
+    def getDestZone(self):
+        return self.__destZone
+
+    def getPatience(self):
+        return self.__patience
+
+    def getDirection(self):
+        return self.__direction
+
+    def getStatus(self):
+        return self.__status
+
+
