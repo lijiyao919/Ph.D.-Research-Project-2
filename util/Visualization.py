bd11482aef6220da576e74d00ccf76961d4566d5
Jiyao Li <jiyao.li@aggiemail.usu.edu>
2020-02-12 10:52:16 -0700

    visulize transit figure

--- util/Visualization.py
+++ util/Visualization.py
@@ -1,15 +1,14 @@
 import pandas as pd
-import datetime
 import numpy as np
 import matplotlib.pyplot as plt
+from src.Configure.Config import *
 
 curr_date = 11
 
 class Visualization:
     def __init__(self):
-        self.begin_time = datetime.datetime(2016, 4, curr_date, hour=0, minute=0)  # year, month and day is not important here
-        self.final_time = datetime.datetime(2016, 4, curr_date, hour=23, minute=45)  # year, month and day is not important here
-        self.delta_time = datetime.timedelta(minutes=15)
+        self.begin_time = 0
+        self.final_time = 479  # 11:45PM
         self.pickup_table = {}
         self.dropoff_table = {}
         self.transition_table = {}
@@ -19,92 +18,82 @@ class Visualization:
         print('Initialize the State Value Table.')
         curr_time = self.begin_time  # 2016-04-01 23:45:00
         while curr_time <= self.final_time:
-            curr_state_t = curr_time.strftime('%m/%d/%Y %H:%M').split(' ')[1]  # e.g. 23:45 (string type)
+            curr_state_t = curr_time
             self.pickup_table[curr_state_t] = np.zeros(78, dtype=np.float64)
             self.dropoff_table[curr_state_t] = np.zeros(78, dtype=np.float64)
-            curr_time = curr_time + self.delta_time
+            curr_time = curr_time + 1
 
-    def __readCSV(self, curr_date):
-        df = pd.read_csv("C:/Users/a02231961/PycharmProjects/Ph.D.-Research-Project-2/data/Chicago_April_11_2016.csv")
-        df['Trip Start Timestamp'] = df['Trip Start Timestamp'].astype('datetime64[ns]') #e.g. 4/4/2016 0:00===> 2016-04-04 00:00:00
-        df['Trip Total'] = df['Trip Total'].str.replace(',', '').astype('float64') #e.g. 1,200===>1200.0
+    def __readCSV(self):
+        df = pd.read_csv(FILENAME_R)
+        df['Fare'] = df['Fare'].astype('float64') #e.g. 1,200===>1200.0
         return df
 
     def handleRequestCount(self):
         print('Count pickup and dropoff in each zone.')
         self.__initilize_table()
 
-        df = self.__readCSV(curr_date)
-        g_time = df.groupby('Trip Start Timestamp')
+        df = self.__readCSV()
+        g_time = df.groupby('Time')
         curr_time = self.final_time
 
         while curr_time >= self.begin_time:
             # clac the reward (current number of requests in each zone)
             pickup_count = np.zeros(78, dtype=np.int16)
             dropoff_count = np.zeros(78, dtype=np.int16)
-            curr_df = g_time.get_group(curr_time.strftime('%m/%d/%Y %H:%M'))
-            curr_state_t = curr_time.strftime('%m/%d/%Y %H:%M').split(' ')[1]
+            curr_df = g_time.get_group(curr_time)
+            curr_state_t = curr_time
 
             row = 0
-            while row < len(curr_df['Pickup Community Area']):
-                pickup_count[curr_df['Pickup Community Area'].iloc[row]] += 1
-                dropoff_count[curr_df['Dropoff Community Area'].iloc[row]] += 1
+            while row < len(curr_df['Pickup']):
+                pickup_count[curr_df['Pickup'].iloc[row]] += 1
+                dropoff_count[curr_df['Dropoff'].iloc[row]] += 1
                 row += 1
             # update the state value
             print('The current time: ', curr_state_t)
             self.pickup_table[curr_state_t] = pickup_count
             self.dropoff_table[curr_state_t] = dropoff_count
 
-            print(curr_df.groupby('Pickup Community Area').size())  # print for compare
-            print(curr_df.groupby('Dropoff Community Area').size())
-            curr_time = curr_time - self.delta_time
+            print(curr_df.groupby('Pickup').size())  # print for compare
+            print(curr_df.groupby('Dropoff').size())
+            curr_time = curr_time - 1
 
     def handleTransitionTable(self, zone_id):
         print('Count from zone_id to the other zones.')
         self.__initilize_table()
 
-        df = self.__readCSV(curr_date)
-        g_time = df.groupby('Trip Start Timestamp')
+        df = self.__readCSV()
+        g_time = df.groupby('Time')
         curr_time = self.final_time
 
         while curr_time >= self.begin_time:
             # clac the reward (current number of requests in each zone)
             dropoff_count = np.zeros(78, dtype=np.int16)
-            curr_df = g_time.get_group(curr_time.strftime('%m/%d/%Y %H:%M'))
-            curr_state_t = curr_time.strftime('%m/%d/%Y %H:%M').split(' ')[1]
+            curr_df = g_time.get_group(curr_time)
+            curr_state_t = curr_time
 
             row = 0
-            while row < len(curr_df['Pickup Community Area']):
-                if curr_df['Pickup Community Area'].iloc[row] == zone_id:
-                    dropoff_count[curr_df['Dropoff Community Area'].iloc[row]] += 1
+            while row < len(curr_df['Pickup']):
+                if curr_df['Pickup'].iloc[row] == zone_id:
+                    dropoff_count[curr_df['Dropoff'].iloc[row]] += 1
                 row += 1
             # update the state value
             print('The current time: ', curr_state_t)
             self.transition_table[curr_state_t] = dropoff_count
 
             print(self.transition_table[curr_state_t])
-            curr_time = curr_time - self.delta_time
+            curr_time = curr_time - 1
+        self.showTransitDesCount()
 
 
-
-    def showCountWRTZones(self, table):
-        print("Show Pickup Number Per Zone Along with time")
-        for zone_id in range(1,78):
-            curr_time = self.begin_time
-            list=[]
-            while curr_time <= self.final_time:
-                curr_state_t = curr_time.strftime('%m/%d/%Y %H:%M').split(' ')[1]
-                list.append(table[curr_state_t][zone_id])
-                curr_time = curr_time + self.delta_time
-            plt.plot(list)
-            plt.title("Zone ID: "+str(zone_id))
-            plt.xticks(range(0,97,4))
-            #plt.savefig('../Figures/zone_{}.jpg'.format(zone_id), dpi=300)
-            plt.show()
+    def showTransitDesCount(self):
+        for time in range(220, self.final_time+1):
+            plt.figure(figsize=(30, 20))
+            plt.bar(range(0,78), self.transition_table[time])
+            plt.title(str(time))
+            plt.xticks(range(0, 78, 1))
+            plt.savefig("C:/Users/a02231961/PycharmProjects/Ph.D.-Research-Project-2/fig1/t_{}.jpg".format(time), dpi=300)
             plt.close()
 
+
 v= Visualization()
-v.handleRequestCount()
-v.showCountWRTZones(v.pickup_table)
-#v.handleTransitionTable(28)
-#v.showCountWRTZones(v.transition_table)
+v.handleTransitionTable(32)
