04c43fa5be0cac1f2ee0943da36da3c442d26098
Jiyao Li <jiyao.li@aggiemail.usu.edu>
2020-06-16 23:16:39 -0600

    make dir setting flexible

--- src/Configure/Config.py
+++ src/Configure/Config.py
@@ -1,5 +1,5 @@
 #Simulation
-FILENAME_D = "C:/Users/a02231961/PycharmProjects/Ph.D.-Research-Project-2/data/Chicago_d_116.csv"
+FILENAME_D = "C:/Users/a02231961/PycharmProjects/Ph.D.-Research-Project-2/data/Chicago_d_316.csv"
 FILENAME_R = "C:/Users/a02231961/PycharmProjects/Ph.D.-Research-Project-2/data/Chicago_r.csv"
 SAVE_PATH = "C:/Users/a02231961/PycharmProjects/Ph.D.-Research-Project-2/fig/p_{}.jpg"
 SIMULATION_CYCLE = 3  #every 3 minutes in one cycle
@@ -23,7 +23,7 @@ DISCOUNT_2 = 1
 DISCOUNT_3 = 1
 DISCOUNT_4 = 1
 
-DIR_THRESHOLD = 30
+DIR_THRESHOLD = 10
 SAT_PRICE = 1
 PATIENCE = 7
 
