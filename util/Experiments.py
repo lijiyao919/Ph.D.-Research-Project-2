d3a1fb7b619aad95b14b6a703c9094e13e6169a0
Jiyao Li <jiyao.li@aggiemail.usu.edu>
2020-06-25 01:37:59 -0600

    update experiment

--- util/Experiments.py
+++ util/Experiments.py
@@ -9,63 +9,55 @@ driver_num = [116, 166, 216, 266, 316]
 serving_rate_str=('20', '30', '40', '50', '60', '70', '80', '90', '100')
 banerjee_w_1= [21, 31, 38, 43, 48]
 banerjee_w_1_pool = [36, 49, 58, 65, 70]
-banerjee_w_learn_pool = [38, 54, 65, 74, 78]
+banerjee_w_learn_pool = [38, 54, 65, 73, 78]
 banerjee_w_learn_pool_idle =[43, 60, 71, 81, 87]
 banerjee_w_learn_pool_idle_learn = [45, 61, 79, 93, 98]'''
 
 #driver revenue
-'''revenue=[800, 1000, 1200, 1400, 1600, 1800, 2000, 2200, 2400]
-revenue_str=('800', '1000', '1200', '1400', '1600', '1800', '2000', '2200', '2400')
-banerjee_w_1= [981, 981, 972, 921, 874]
-banerjee_w_1_pool = [1893, 1940, 1816, 1634, 1478]
-banerjee_w_learn_pool = [1945, 2156, 1966, 1826, 1638]
-banerjee_w_learn_pool_idle =[2334, 2421, 2182, 2000, 1822]
-banerjee_w_learn_pool_idle_learn = [2389, 2496, 2381, 2298, 2075]'''
+'''revenue=[800, 1000, 1200, 1400, 1600, 1800, 2000]
+revenue_str=('800', '1000', '1200', '1400', '1600', '1800', '2000')
+banerjee_w_1= [842.61, 859.95, 861.75, 818.38, 776.87]
+banerjee_w_1_pool = [1592.42, 1620.29, 1534.34, 1380.65, 1250.76]
+banerjee_w_learn_pool = [1680.04, 1834.25, 1676.67, 1546.63, 1402.58]
+banerjee_w_learn_pool_idle =[1976.09, 2038.54, 1819.99, 1682.49, 1523.09]
+banerjee_w_learn_pool_idle_learn = [2030.41, 2089.88, 1996.61, 1894.9, 1677.12]'''
 
 #driver_effort
 '''effort=[140, 160, 180, 200, 220, 240]
 effort_str=('140', '160', '180', '200', '220', '240')
 banerjee_w_1= [173, 170, 164, 153, 144]
-banerjee_w_1_pool = [194, 184, 168, 151, 137]
-banerjee_w_learn_pool = [197, 199, 182, 168, 151]
-banerjee_w_learn_pool_idle =[233, 228, 213, 204, 188]
+banerjee_w_1_pool = [193, 183, 168, 151, 137]
+banerjee_w_learn_pool = [198, 199, 182, 167, 152]
+banerjee_w_learn_pool_idle =[234, 228, 212, 204, 188]
 banerjee_w_learn_pool_idle_learn = [240, 239, 238, 235, 225]'''
 
 #driver profit
-'''profit=[600, 800, 1000, 1200, 1400, 1600, 1800, 2000]
-profit_str=('600', '800', '1000', '1200', '1400', '1600', '1800', '2000')
-banerjee_w_1= [635, 639, 644, 615, 586]
-banerjee_w_1_pool = [1505, 1572, 1480, 1331, 1203]
-banerjee_w_learn_pool = [1551, 1758, 1603, 1489, 1335]
-banerjee_w_learn_pool_idle =[1867, 1966, 1756, 1592, 1446]
-banerjee_w_learn_pool_idle_learn = [1908, 2016, 1904, 1829, 1625]'''
+'''profit=[600, 800, 1000, 1200, 1400, 1600]
+profit_str=('600', '800', '1000', '1200', '1400', '1600')
+banerjee_w_1= [496.26, 518.1, 534.07, 512.04, 488.74]
+banerjee_w_1_pool = [1206.42, 1254.01, 1198.2, 1078.4, 976.4]
+banerjee_w_learn_pool = [1284.26, 1435.49, 1312.96, 1211.75, 1099.26]
+banerjee_w_learn_pool_idle =[1508.43, 1582.92, 1395.01, 1273.93, 1146.27]
+banerjee_w_learn_pool_idle_learn = [1550.2, 1611.34, 1520.25, 1424.51, 1227.47]'''
 
 #driver utilization
 '''utilization=[50, 60, 70, 80, 90, 100]
 utilization_str=('50', '60', '70', '80', '90', '100')
 banerjee_w_1= [86, 83, 78, 73, 68]
-banerjee_w_1_pool = [78, 74, 67, 59, 53]
+banerjee_w_1_pool = [78, 73, 67, 59, 53]
 banerjee_w_learn_pool = [79, 79, 72, 66, 59]
-banerjee_w_learn_pool_idle =[90, 88, 79, 71, 64]
+banerjee_w_learn_pool_idle =[90, 87, 79, 72, 64]
 banerjee_w_learn_pool_idle_learn = [93, 90, 86, 80, 71]'''
 
 #rider call time
 '''call=[0.5, 1, 1.5, 2, 2.5, 3, 3.5]
 call_str=('0.5', '1', '1.5', '2', '2.5', '3', '3.5')
 banerjee_w_1= [3.48, 3, 2.7, 2.4, 2.2]
-banerjee_w_1_pool = [2.88, 2.5, 1.98, 1.6, 1.4]
-banerjee_w_learn_pool = [2.82, 2.34, 1.92, 1.5, 1.1]
+banerjee_w_1_pool = [2.88, 2.5, 1.98, 1.5, 1.4]
+banerjee_w_learn_pool = [2.84, 2.34, 1.95, 1.5, 1.1]
 banerjee_w_learn_pool_idle =[2.8, 2.2, 1.8, 1.4, 1]
-banerjee_w_learn_pool_idle_learn = [2.8, 2.1, 1.8, 1.2, 0.54]'''
+banerjee_w_learn_pool_idle_learn = [2.8, 2.1, 1.8, 1.2, 0.57]'''
 
-#rider detour time
-'''detour=[0.5, 1, 1.5, 2, 2.5, 3, 3.5]
-detour_str=('0.5', '1', '1.5', '2', '2.5', '3', '3.5')
-banerjee_w_1= [2.55, 2.2, 2.0, 2.0, 2.0]
-banerjee_w_1_pool = [2.7, 2.6, 2.6, 2.5, 2.5]
-banerjee_w_learn_pool = [2.55, 2.5, 2.4, 2.4, 2.3]
-banerjee_w_learn_pool_idle =[2.5, 2.55, 2.55, 2.5, 2.5]
-banerjee_w_learn_pool_idle_learn = [2.5, 2.6, 2.5, 2.6, 2.8]'''
 
 #code
 '''fig, ax = plt.subplots()
@@ -80,7 +72,7 @@ ax.set_xticklabels(('116', '166', '216', '266', '316'), fontsize=frontsize)
 
 ax.set_yticks(call)
 ax.set_yticklabels(call_str, fontsize=frontsize)
-ax.set_ylabel('The Average Calling Time of Rider', fontsize=frontsize)
+ax.set_ylabel('The Average Calling Time of Rider (min)', fontsize=frontsize)
 ax.legend(prop={'size': 11})
 plt.show()'''
 
@@ -107,7 +99,7 @@ ax.legend(prop={'size': 11})
 plt.show()'''
 
 
-angle_num = [5, 10 ,20 ,30 ,40]
+'''angle_num = [5, 10 ,20 ,30 ,40]
 detour=[2, 2.2, 2.5, 2.8, 3]
 detour_str=('2', '2.2', '2.5', '2.8', '3')
 wait=[2.1, 2.3, 2.5, 2.7, 3]
@@ -120,7 +112,7 @@ ax.set_xticklabels(('5', '10', '20', '30', '40'), fontsize=frontsize)
 ax.set_yticks(detour)
 ax.set_yticklabels(detour_str, fontsize=frontsize)
 ax.set_ylabel('The Average Detour Time of Rider', fontsize=frontsize)
-plt.show()
+plt.show()'''
 
 
 
