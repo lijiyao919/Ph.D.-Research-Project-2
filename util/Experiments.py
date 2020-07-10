d3b8bc9e61b995c315c3d993115212edf691bb88
Jiyao Li <jiyao.li@aggiemail.usu.edu>
2020-07-10 00:11:29 -0600

    experiment updated

--- util/Experiments.py
+++ util/Experiments.py
@@ -5,8 +5,8 @@ frontsize=14
 driver_num = [116, 166, 216, 266, 316]
 
 #serving rate
-'''serving_rate=[20, 30, 40, 50, 60, 70, 80, 90, 100]
-serving_rate_str=('20', '30', '40', '50', '60', '70', '80', '90', '100')
+'''serving_rate=[20, 30, 40, 50, 60, 70, 80, 90]
+serving_rate_str=('20', '30', '40', '50', '60', '70', '80', '90')
 banerjee_w_1= [21, 31, 38, 43, 48]
 banerjee_w_1_pool = [36, 49, 58, 65, 70]
 banerjee_w_learn_pool = [38, 54, 65, 73, 78]
@@ -32,17 +32,17 @@ banerjee_w_learn_pool_idle =[234, 228, 212, 204, 188]
 banerjee_w_learn_pool_idle_learn = [240, 239, 238, 235, 225]'''
 
 #driver profit
-profit=[600, 800, 1000, 1200, 1400, 1600]
+'''profit=[600, 800, 1000, 1200, 1400, 1600]
 profit_str=('600', '800', '1000', '1200', '1400', '1600')
 banerjee_w_1= [496.26, 518.1, 534.07, 512.04, 488.74]
 banerjee_w_1_pool = [1206.42, 1254.01, 1198.2, 1078.4, 976.4]
 banerjee_w_learn_pool = [1284.26, 1435.49, 1312.96, 1211.75, 1099.26]
 banerjee_w_learn_pool_idle =[1508.43, 1582.92, 1395.01, 1273.93, 1146.27]
-banerjee_w_learn_pool_idle_learn = [1550.2, 1611.34, 1520.25, 1424.51, 1227.47]
+banerjee_w_learn_pool_idle_learn = [1550.2, 1611.34, 1520.25, 1424.51, 1227.47]'''
 
 #driver utilization
-'''utilization=[50, 60, 70, 80, 90, 100]
-utilization_str=('50', '60', '70', '80', '90', '100')
+'''utilization=[50, 60, 70, 80, 90]
+utilization_str=('50', '60', '70', '80', '90')
 banerjee_w_1= [86, 83, 78, 73, 68]
 banerjee_w_1_pool = [78, 73, 67, 59, 53]
 banerjee_w_learn_pool = [79, 79, 72, 66, 59]
@@ -50,13 +50,13 @@ banerjee_w_learn_pool_idle =[90, 87, 79, 72, 64]
 banerjee_w_learn_pool_idle_learn = [93, 90, 86, 80, 71]'''
 
 #rider call time
-'''call=[0.5, 1, 1.5, 2, 2.5, 3, 3.5]
-call_str=('0.5', '1', '1.5', '2', '2.5', '3', '3.5')
+call=[1, 1.5, 2, 2.5, 3, 3.5]
+call_str=( '1', '1.5', '2', '2.5', '3', '3.5')
 banerjee_w_1= [3.48, 3, 2.7, 2.4, 2.2]
 banerjee_w_1_pool = [2.88, 2.5, 1.98, 1.5, 1.4]
 banerjee_w_learn_pool = [2.84, 2.34, 1.95, 1.5, 1.1]
 banerjee_w_learn_pool_idle =[2.8, 2.2, 1.8, 1.4, 1]
-banerjee_w_learn_pool_idle_learn = [2.8, 2.1, 1.8, 1.2, 0.57]'''
+banerjee_w_learn_pool_idle_learn = [2.8, 2.1, 1.8, 1.2, 0.57]
 
 
 #code
@@ -64,15 +64,15 @@ fig, ax = plt.subplots()
 ax.plot(driver_num, banerjee_w_1, 'cd-', label='SMW')
 ax.plot(driver_num, banerjee_w_1_pool, 'gp-', label='SMW+CP')
 ax.plot(driver_num, banerjee_w_learn_pool, 'b*-', label='ARDL+CP')
-ax.plot(driver_num, banerjee_w_learn_pool_idle, 'r^-', label='ARDL+CP+GIM')
-ax.plot(driver_num, banerjee_w_learn_pool_idle_learn, 'ko-', label='ARDL+CP+LIM')
+ax.plot(driver_num, banerjee_w_learn_pool_idle, 'ko-', label='ARDL+CP+GIM')
+#ax.plot(driver_num, banerjee_w_learn_pool_idle_learn, 'ko-', label='ARDL+CP+LIM')
 ax.set_xlabel('The Number of Taxi', fontsize=frontsize)
 ax.set_xticks(driver_num)
 ax.set_xticklabels(('116', '166', '216', '266', '316'), fontsize=frontsize)
 
-ax.set_yticks(profit)
-ax.set_yticklabels(profit_str, fontsize=frontsize)
-ax.set_ylabel('The Average Profit of Taxi Driver ($)', fontsize=frontsize)
+ax.set_yticks(call)
+ax.set_yticklabels(call_str, fontsize=frontsize)
+ax.set_ylabel('The Average Calling Time of Rider (min)', fontsize=frontsize)
 ax.legend(prop={'size': 11})
 plt.show()
 
@@ -85,16 +85,16 @@ pool_in_3 = [13.1, 14.8, 14.1, 12.7, 11.5]
 pool_in_2 = [22, 20.7, 17.3, 13.8, 12.1]
 pool_in_1 = [38.5, 29.6, 21.1, 17.2, 15.1]
 fig, ax = plt.subplots()
-ax.plot(angle_num, pool_in_4, 'gp-', label='% of riders pooling in 4')
-ax.plot(angle_num, pool_in_3, 'b*-', label='% of riders pooling in 3')
-ax.plot(angle_num, pool_in_2, 'r^-', label='% of riders pooling in 2')
-ax.plot(angle_num, pool_in_1, 'ko-', label='% of riders pooling in 1')
-ax.set_xlabel('The Threshold of CTR', fontsize=frontsize)
+ax.plot(angle_num, pool_in_4, 'gp-', label='Poolability at Level 4')
+ax.plot(angle_num, pool_in_3, 'b*-', label='Poolability at Level 3')
+ax.plot(angle_num, pool_in_2, 'r^-', label='Poolability at Level 2')
+ax.plot(angle_num, pool_in_1, 'ko-', label='Poolability at Level 1')
+ax.set_xlabel(r"$\theta_{CTR}$", fontsize=15)
 ax.set_xticks(angle_num)
 ax.set_xticklabels(('5', '10', '20', '30', '40'), fontsize=frontsize)
 ax.set_yticks(pool_rate)
 ax.set_yticklabels(pool_rate_str, fontsize=frontsize)
-ax.set_ylabel('The Average Pooling Rate (%)', fontsize=frontsize)
+ax.set_ylabel('The Poolability (%)', fontsize=frontsize)
 ax.legend(prop={'size': 11})
 plt.show()'''
 
@@ -111,7 +111,7 @@ fig, ax = plt.subplots()
 ax2 = ax.twinx()
 ax.plot(angle_num, wait, 'ko-', label='Extra Trip Time')
 ax2.plot(angle_num, save, 'bd-',label='Saved Money')
-ax.set_xlabel('The Threshold of CTR', fontsize=frontsize)
+ax.set_xlabel(r"$\theta_{CTR}$", fontsize=15)
 ax.set_xticks(angle_num)
 ax.set_xticklabels(('5', '10', '20', '30', '40'), fontsize=frontsize)
 
@@ -121,8 +121,8 @@ ax.set_ylabel('The Average Extra Trip Time of Rider (min)', fontsize=frontsize)
 ax2.set_yticks(save)
 ax2.set_yticklabels(save_str, fontsize=frontsize)
 ax2.set_ylabel('The Average Saved Money of Rider ($)', color='b', fontsize=frontsize)
-ax.legend(prop={'size': 11}, loc='lower right')
-ax2.legend(prop={'size': 11}, loc='upper left')
+ax.legend(prop={'size': 13}, loc='lower right')
+ax2.legend(prop={'size': 13}, loc='upper left')
 plt.show()'''
 
 
