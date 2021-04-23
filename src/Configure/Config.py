d1c9146557db8e87b6eb4cd7ac0e434a7dbc9dd9
Jiyao Li <jiyao.li@aggiemail.usu.edu>
2021-04-22 23:24:57 -0600

    next day

--- src/Configure/Config.py
+++ src/Configure/Config.py
@@ -1,12 +1,20 @@
+#Util For generate
+REQ_DATE = 12
+REQ_NUM = 62019
+FILENAME_Chicago = "C:/Users/a02231961/PycharmProjects/Ph.D.-Research-Project-2/data/Chicago_April_" + str(REQ_DATE) + "_2016.csv"
+POPULARITY_SCORE_FILE = 'C:/Users/a02231961/PycharmProjects/Ph.D.-Research-Project-2/data/data_2.json'
+BEGIN_TIME = 0
+END_TIME = 479
+
 #Simulation
 FILENAME_D = "C:/Users/a02231961/PycharmProjects/Ph.D.-Research-Project-2/data/Chicago_d_316.csv"
-FILENAME_R = "C:/Users/a02231961/PycharmProjects/Ph.D.-Research-Project-2/data/Chicago_r.csv"
+FILENAME_R = "C:/Users/a02231961/PycharmProjects/Ph.D.-Research-Project-2/data/Chicago_r_4_" + str(REQ_DATE) + "_2016.csv"
 SAVE_PATH = "C:/Users/a02231961/PycharmProjects/Ph.D.-Research-Project-2/fig/p_{}.jpg"
 SIMULATION_CYCLE = 3  #every 3 minutes in one cycle
 SIMULATION_CYCLE_START = 220
 SIMULATION_CYCLE_END = 491
-RIDER_ROW_START = 16250 #11AM
-RIDER_ROW_END = 57172 # 23:45PM
+RIDER_ROW_START = 18255 #16250 #11AM
+RIDER_ROW_END = 62019 #57172 # 23:45PM
 SHOWN_INTERVAL = 490 #10
 BENERJEE_W_1 = False
 
@@ -23,7 +31,7 @@ DISCOUNT_2 = 1
 DISCOUNT_3 = 1
 DISCOUNT_4 = 1
 
-DIR_THRESHOLD = 40
+DIR_THRESHOLD = 30
 SAT_PRICE = 1
 PATIENCE = 7
 
@@ -37,9 +45,6 @@ DROPOFF="dropoff"
 #IDLE_MOVE_THRE_LEARN = True
 
 #Util
-FILENAME_Chicago = "C:/Users/a02231961/PycharmProjects/Ph.D.-Research-Project-2/data/Chicago_April_11_2016.csv"
-BEGIN_TIME = 0
-END_TIME = 479
 SMOOTH_RATOR = 0.5
 BASE_ZONE = 32
 
